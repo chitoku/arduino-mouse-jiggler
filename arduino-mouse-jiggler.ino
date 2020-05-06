29f91b621a356f6db4eed20ae600070d597dcd3d
chitoku <chitoku@gmail.com>
2020-05-05 22:18:42 -0700

    Add LED to signal mouse jiggling

--- arduino-mouse-jiggler.ino
+++ arduino-mouse-jiggler.ino
@@ -8,15 +8,19 @@
 
 int distance = 200;           // number of pixels the mouse travels
 int strokeDelay = 500;        // delay of the mouse (in ms) after each stroke
-int jiggleInterval = 1000;    // interval between the jiggle (set of strokes) in ms
+int jiggleInterval = 60000;    // interval between the jiggle (set of strokes) in ms
 
 void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
   // initialize mouse control:
   Mouse.begin();
 }
 
 void loop() {
-
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second before starting the jiggle
+  
   Mouse.move(distance, 0, 0);
   delay(strokeDelay);
   Mouse.move(0, distance, 0);
@@ -25,6 +29,8 @@ void loop() {
   delay(strokeDelay);
   Mouse.move(0, -distance, 0);
   delay(strokeDelay);
+  
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
 
   // Mouse jiggle happens every {jiggleInterval}/1000 second
   delay(jiggleInterval);
