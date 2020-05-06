373f68372d46b532330323669b88e3606fd45cca
chitoku <chitoku@gmail.com>
2020-05-05 21:40:54 -0700

    Initial commit

---
+++ arduino-mouse-jiggler.ino
@@ -0,0 +1,31 @@
+/*
+  Arduino Mouse Jiggler
+
+  For Leonardo and Due boards only.
+*/
+
+#include "Mouse.h"
+
+int distance = 200;           // number of pixels the mouse travels
+int strokeDelay = 500;        // delay of the mouse (in ms) after each stroke
+int jiggleInterval = 1000     // interval between the jiggle (set of strokes) in ms
+
+void setup() {
+  // initialize mouse control:
+  Mouse.begin();
+}
+
+void loop() {
+
+  Mouse.move(distance, 0, 0);
+  delay(strokeDelay);
+  Mouse.move(distance, 0, 0);
+  delay(strokeDelay);
+  Mouse.move(distance, 0, 0);
+  delay(strokeDelay);
+  Mouse.move(distance, 0, 0);
+  delay(strokeDelay);
+
+  // Mouse jiggle happens every {jiggleInterval}/1000 second
+  delay(jiggleInterval);
+}
